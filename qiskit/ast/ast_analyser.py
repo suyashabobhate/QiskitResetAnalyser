58674a89d5fd50da60f9711739b6fdceba212aea
suyasha bobhate <suyashabobhate@gmail.com>
2024-11-12 12:16:02 -0800

    Fix for loop traversal

--- qiskit/ast/ast_analyser.py
+++ qiskit/ast/ast_analyser.py
@@ -8,23 +8,26 @@ class ResetCheckAnalyzer(ast.NodeVisitor):
         self.used_qubits = {}  # Track which qubits have been used
         self.assigned_vars = {}
         self.op_name = None
+        self.inside_for = False
 
     # Function to add used qubits to the dict after applying any operations
-    def apply_op(self, lower_limit, upper_limit):
-        for qubit in range(lower_limit, upper_limit):
-            # Found any operations like gates
-            if self.op_name in ('h', 'x', 'measure', 'cx'):
-                if qubit in self.used_qubits and self.used_qubits[qubit] != self.current_term:
-                    print(f"Error: Qubit {qubit} is being reused without reset")
-                # Found unused qubit or resetted qubit
-                else:
-                    self.used_qubits[qubit] = self.current_term
-        
-            # Found reset operation
-            elif self.op_name == 'reset':
-                # Reset qubits by deleting them from the dict
-                if qubit in self.used_qubits:
-                    del self.used_qubits[qubit]
+    def apply_op(self, qubit):
+        # for qubit in range(lower_limit, upper_limit):
+        # Found any operations like gates
+        if self.op_name in ('h', 'x', 'measure', 'cx'):
+            if qubit in self.used_qubits and self.used_qubits[qubit] != self.current_term:
+                print(f"Error: Qubit {qubit} is being reused without reset")
+            # Found unused qubit or resetted qubit
+            else:
+                self.used_qubits[qubit] = self.current_term
+                print(f"Qubit {qubit} is assigned operation {self.op_name} in term {self.current_term}")
+    
+        # Found reset operation
+        elif self.op_name == 'reset':
+            # Reset qubits by deleting them from the dict
+            if qubit in self.used_qubits:
+                print(f"Qubit {qubit} is resetted to use in term {self.current_term}")
+                del self.used_qubits[qubit]
 
     # Get operation name
     def getOpName(self, node):
@@ -37,6 +40,18 @@ class ResetCheckAnalyzer(ast.NodeVisitor):
         elif isinstance(node, ast.Call) and isinstance(node.func, ast.Attribute):
             self.op_name = node.func.attr
             self.assigned_vars["op_name"] = self.op_name
+
+    def checkType(self, node_value):
+        if isinstance(node_value, ast.Constant):
+            var_value = node_value.value
+        elif isinstance(node_value, ast.Name):
+            if self.assigned_vars[node_value.id]:
+                var_name = node_value.id
+                var_value = self.assigned_vars[var_name]
+        else:
+            return -1
+
+        return var_value
             
     def visit_Assign(self, node):
         # Store all assigned variable values
@@ -50,24 +65,29 @@ class ResetCheckAnalyzer(ast.NodeVisitor):
         
         self.generic_visit(node)
 
-    # TO DO: add functionality for range(lower, upper) :- args[0], args[1] and for formula ranges :- size - 1
     def visit_For(self, node):
-        # Go over every statement in the for loop
-        for node_value in node.body: 
-            # Get the name of the operation
-            self.getOpName(node_value)
-            if isinstance(node.iter, ast.Call) and node.iter.func.id == "range":
-                # The range of for loop is between 0 and an integer
-                if node.iter.args and isinstance(node.iter.args[0], ast.Constant):
-                    var_value = node.iter.args[0].value
-                    self.apply_op(0, var_value)
-                # Range of for loop is between 0 and value of an variable
-                elif node.iter.args and isinstance(node.iter.args[0], ast.Name):
-                    if self.assigned_vars[node.iter.args[0].id]:
-                        var_name = node.iter.args[0].id
-                        self.apply_op(0, self.assigned_vars[var_name])
-                    
+        self.inside_for = True
+        if isinstance(node.iter, ast.Call) and node.iter.func.id == "range":
+                if node.iter.args and len(node.iter.args) == 1:
+                    lower_limit = 0
+                    upper_limit = self.checkType(node.iter.args[0])
+                elif node.iter.args and len(node.iter.args) == 2:
+                    lower_limit = self.checkType(node.iter.args[0])
+                    upper_limit = self.checkType(node.iter.args[1])
+
+        if lower_limit != -1 and upper_limit != -1:
+            for qubit in range(lower_limit, upper_limit):
+                # Go over every statement in the for loop
+                for node_value in node.body: 
+                    # Get the name of the operation
+                    self.getOpName(node_value)
+                    if isinstance(node_value.value.args[0], ast.Constant):
+                        self.apply_op(node_value.value.args[0].value)
+                    else:
+                        self.apply_op(qubit)
+
         self.generic_visit(node)
+        self.inside_for = False
 
     def visit_Call(self, node):
         # Get size of the circuit
@@ -89,11 +109,12 @@ class ResetCheckAnalyzer(ast.NodeVisitor):
         # Increase term everytime run is operated
         if self.op_name == 'run':
             self.current_term += 1
+            print(f"Updated new term is {self.current_term}")
         else:
             # Check for other operations in single statement
             if len(node.args) > 0 and isinstance(node.func, ast.Attribute):
-                if isinstance(node.args[0], ast.Constant) and isinstance(node.args[0].value, int):
-                    self.apply_op(node.args[0].value, node.args[0].value + 1)
+                if not self.inside_for and isinstance(node.args[0], ast.Constant) and isinstance(node.args[0].value, int):
+                    self.apply_op(node.args[0].value)
                     
         self.generic_visit(node)
 
